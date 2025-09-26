f17c95641324a8228d5025c64783a27e3e9b5c51
c9088610 <c9088610@gmail.com>
2025-09-26 10:09:20 +0300

    Events

---
+++ EventApi/Controllers/EventsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+
+        public List<Event> events = new List<Event> { new Event { Id=1,Title="wedding",Start=new DateTime()},
+            new Event { Id=2,Title="bar mitzva", Start=new DateTime()} };
+
+        // GET: api/<EventsController>
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return events;
+        }
+
+        // GET api/<EventsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EventsController>
+        [HttpPost]
+        public void Post([FromBody] Event value)
+        {
+
+        }
+
+        // PUT api/<EventsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EventApi/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace EventApi
+{
+    public class Event
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+    }
+}
